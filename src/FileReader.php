d4fac1b87e2d9b66826ece4f11d129ddc30c5025
danieliser <danieliser@wizardinternetsolutions.com>
2020-07-04 19:14:21 -0400

    Reduce overhead by only loading file header
    Rewrites the getPluginVersion method to no longer load the entire plugin
    file but rather only the first 8kb header, mimicing WordPress core.
    
    This should be much better for memory usage and overall package
    performance.
    
    Closes #2

--- src/FileReader.php
+++ src/FileReader.php
@@ -6,9 +6,12 @@ class FileReader
 {
     public function getPluginVersion($slug)
     {
-        $contents = file_get_contents(WP_PLUGIN_DIR .'/' . $slug);
-        $matches = [];
-        preg_match('/(?:.*)(?:Version:)\s*(.*)?/', $contents, $matches);
-        return $matches[1];
+	    // Stripped down list of headers.
+	    $default_headers = [
+		    'Version'     => 'Version',
+	    ];
+
+	    $plugin_data = get_file_data( WP_PLUGIN_DIR . '/' . $slug, $default_headers, 'plugin' );
+	    return ! empty( $plugin_data['Version'] ) ? $plugin_data['Version'] : false; // Set default that you are set to handle as a false.
     }
 }
